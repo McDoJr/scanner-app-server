d6e8165e50e26cc8bf2595b216f7f138098fb3fe
McDoJr <user@MacBookJr.local>
2024-12-09 13:14:03 +0800

    initial commit

--- server.js
+++ server.js
@@ -137,5 +137,6 @@ app.post('/predict', async (req, res) => {
     }
 });
 
+const HOST = '0.0.0.0';
 const PORT = process.env.PORT;
-app.listen(PORT, () => console.log("Server running on port " + PORT));
+app.listen(PORT, HOST, () => console.log("Server running on " + (HOST) + ":" + PORT));
